a187daee03e5c1edade6eef42589c68bcec0b2e4
liemlylac <liemlylac@gmail.com>
2020-03-14 14:09:03 +0700

    ✨ Add OpenAPI (Swagger) #5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Crawler Data')
+    .setDescription('Using nest-crawler to crawl data for personal usage')
+    .setVersion('0.1.0')
+    .addTag('nest-crawler-data')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
